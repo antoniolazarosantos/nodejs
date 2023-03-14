36a560673660fbfe655fabbf278bc642a69ab140
antoniolazarosantos <antoniolazarosantos@gmail.com>
2023-03-14 18:40:39 -0300

    Argumentos da linha de comando

---
+++ ARQUIVOS/2_FUNDAMENTOS/4_ler_argumentos/index.js
@@ -0,0 +1,10 @@
+// linha de comando: node index.js nome=Lázaro
+
+console.log(process.argv);
+
+const args = process.argv.slice(2);
+console.log(args);
+
+const name = args[0].split("=")[1];
+
+console.log(name);
