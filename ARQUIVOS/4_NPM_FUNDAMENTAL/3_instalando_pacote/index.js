2d73000872efe0997093404a05023df58165f6db
antoniolazarosantos <antoniolazarosantos@gmail.com>
2023-05-02 18:01:55 -0300

    Criando projeto com npm init

--- ARQUIVOS/4_NPM_FUNDAMENTAL/3_instalando_pacote/index.js
+++
@@ -1,7 +0,0 @@
-const _ = require("lodash");
-const a = [1,5,6,9,0];
-const b = [7,3,4,1,0];
-const diff = _.difference(a,b);
-console.log(diff);
-const diff2 = _.difference(b,a);
-console.log(diff2);
