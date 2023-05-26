1f71e8b8fbfa47fbf0dd9097e88fbc20da3893c5
antoniolazarosantos <antoniolazarosantos@gmail.com>
2023-05-26 17:36:35 -0300

    Criando uma página e rota 404

---
+++ ARQUIVOS/6_EXPRESS/10_404/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require('path');
+const basePath = path.join(__dirname,'templates');
+const usersRouter = require('./users'); // Importa o index.js , já é uma notação padrão do Node
+
+// ler o body 
+// necessário para pegar os dados via POST
+
+app.use(
+    express.urlencoded({extended: true,})
+);
+
+app.use(express.json());
+
+// Arquivos estáticos
+app.use(express.static("public"));
+
+app.use('/users',usersRouter); // Tem que estar acima da rota principal
+
+// Rota principal
+app.get('/',(req,res) => {
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.use(function(res,res,next){
+    res.status(404).sendFile(`${basePath}/404.html`);
+});
+
+app.listen(port,()=>{
+ console.log(`App rodando na porta ${port}`);
+});
