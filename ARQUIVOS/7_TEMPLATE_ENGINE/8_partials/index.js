d03f746dcbf1a0ff82f68169ddeb3fd565940e4b
antoniolazarosantos <antoniolazarosantos@gmail.com>
2023-06-10 09:43:20 -0300

    Conhecendo os partials

--- ARQUIVOS/7_TEMPLATE_ENGINE/8_partials/index.js
+++ ARQUIVOS/7_TEMPLATE_ENGINE/8_partials/index.js
@@ -4,8 +4,13 @@ const exphbs = require('express-handlebars');
 const app = express();
 
 //Setup do handlebars ( template engine)
-app.engine('handlebars',exphbs.engine());
+const hbs = exphbs.create({
+    partialsDir: ["views/partials/"],
+  });
+
+app.engine('handlebars',hbs.engine);
 app.set('view engine','handlebars');
+
 
 app.get('/dashboard',(req,res)=>{
     const items = [{"id":1,"descricao":"Laranja"},{"id":2,"descricao":"Melão"},{"id":3,"descricao":"Abacate"}];
@@ -21,6 +26,31 @@ app.get("/post", function (req, res) {
     };
   
     res.render("blogpost", { post });
+  });
+
+  app.get("/blog", function (req, res) {
+    const posts = [
+      {
+        title: "Aprender Node.js",
+        category: "Node.js",
+        body: "Node.js é muito utilizado na programação hoje em dia",
+        comments: 4,
+      },
+      {
+        title: "PHP ainda vale a pena?",
+        category: "PHP",
+        body: "",
+        comments: 12,
+      },
+      {
+        title: "Os segredos de JavaScript",
+        category: "JavaScript",
+        body: "",
+        comments: 5,
+      },
+    ];
+  
+    res.render("blog", { posts });
   });
 
 // rota principal
